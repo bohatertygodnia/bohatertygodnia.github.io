ba8d3aea13c0860dbc0171fa38ba3983b0422f81
bohatertygodnia <32752778+bohatertygodnia@users.noreply.github.com>
2017-11-06 18:31:59 +0100

    Dodane nowe ikony

--- js/data.js
+++ js/data.js
@@ -53,23 +53,23 @@ var expertMissions = [
 	{ id:'5',	name: 'Opróżnić zmywarkę',		icon: 'assets/ikony/m_zmywarka.svg'	},	
 	{ id:'6',	name: 'Wstawić pranie',			icon: 'assets/laundry.svg'		},
 	{ id:'7',	name: 'Pościelić łóżko',		icon: 'assets/ikony/m_lozko.svg'			},
-	{ id:'8',	name: 'Odkurzyć podłogę',		icon: 'assets/vacuum.svg'		},
+	{ id:'8',	name: 'Odkurzyć podłogę',		icon: 'assets/ikony/m_odkurzacz.svg'		},
 	{ id:'9',	name: 'Umyć podłogę',			icon: 'assets/cleaning.svg'		},	
 	{ id:'10',	name: 'Nakryć stół',			icon: 'assets/cutlery.svg'		},
 	{ id:'11',	name: 'Posprzątać ze stołu',	icon: 'assets/table.svg'		},	
 	{ id:'12',	name: 'Wynieść śmieci',			icon: 'assets/ikony/m_smieci.svg'		},	
 	{ id:'13',	name: 'Posprzątać po zwierzątku',icon: 'assets/ikony/m_kuweta.svg'		},	
-	{ id:'14',	name: 'Umyć auto',				icon: 'assets/car.svg'			},	
+	{ id:'14',	name: 'Umyć auto',				icon: 'assets/ikony/m_samochod.svg'			},	
 	{ id:'15',	name: 'Zetrzeć kurze',			icon: 'assets/ikony/m_kurz.svg'		},	
 	{ id:'16',	name: 'Nakarmić zwierzątko',	icon: 'assets/ikony/m_nakarmic.svg'		},	
 	{ id:'17',	name: 'Wyprowadzić zwierzątko',	icon: 'assets/ikony/m_spacer.svg'			},	
 	{ id:'18',	name: 'Rozpakować zakupy',		icon: 'assets/ikony/m_zakupy.svg'		},	
 	{ id:'19',	name: 'Pomoc w ogródku',		icon: 'assets/ikony/m_ogrod.svg'			},	
-	{ id:'20',	name: 'Umyć naczynia',			icon: 'assets/wash.svg'			},	
+	{ id:'20',	name: 'Umyć naczynia',			icon: 'assets/ikony/m_naczynia.svg'			},	
 	{ id:'21',	name: 'Odrobić pracę domową',	icon: 'assets/ikony/m_lekcje.svg'	},	
 	{ id:'22',	name: 'Przeczytać książkę',		icon: 'assets/book.svg'			},		
 	{ id:'23',	name: 'Poskładać ubrania',		icon: 'assets/ikony/m_ubrania.svg'		},
-	{ id:'24',	name: 'Ubrać się samemu',		icon: 'assets/shirt.svg'		}	
+	{ id:'24',	name: 'Ubrać się samemu',		icon: 'assets/ikony/m_ubrac.svg'		}	
 ]
 
 var userMissions = [
@@ -142,7 +142,7 @@ var doneMissions = [
 var expertGifts = [
 	{ id:'1',	name: 'Wyjście na lody', icon: 'assets/ikony/n_lody.svg'},
 	{ id:'2',	name: 'Wyjście do kina', icon: 'assets/ikony/n_kino.svg'	},
-	{ id:'3',	name: 'Wycieczka na weekend', icon: 'assets/walk.svg'},
+	{ id:'3',	name: 'Wycieczka na weekend', icon: 'assets/ikony/n_wycieczka.svg'},
 	{ id:'4',	name: 'Aquapark ', icon: 'assets/swimming-pool.svg'},
 	{ id:'5',	name: '15 min na gry', icon: 'assets/ikony/n_kwadrans.svg'},
 	{ id:'6',	name: '30 min na gry', icon: 'assets/ikony/n_pol.svg'},
